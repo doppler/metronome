60d29445a9b121707423606311db6bc9f7500b85
David Rose <doppler@gmail.com>
2020-04-21 21:03:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Transport, Loop, MembraneSynth } from 'tone';
 import './App.css';
 
+const synth = new MembraneSynth({
+  octaves: 1.08,
+  pitchDecay: 0.0125,
+  envelope: { release: 0.2 },
+}).toMaster();
+
+new Loop((time) => {
+  synth.triggerAttackRelease('C5', '32n');
+}, '4n').start(0);
+
 function App() {
+  const [bpm, setBpm] = useState(120);
+  const [isPlaying, setPlaying] = useState(false);
+
+  const handleKeyPress = useCallback(({ key, keyCode }) => {
+    switch (key) {
+      case 'ArrowUp':
+        setBpm((value) => value + 1);
+        break;
+      case 'ArrowDown':
+        setBpm((value) => value - 1);
+        break;
+      case 'ArrowRight':
+        setBpm((value) => value + 10);
+        break;
+      case 'ArrowLeft':
+        setBpm((value) => value - 10);
+        break;
+      default:
+        break;
+    }
+    if (keyCode === 32) togglePlay();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, [handleKeyPress]);
+
+  useEffect(() => {
+    isPlaying ? Transport.start() : Transport.stop();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    Transport.bpm.value = bpm;
+  }, [bpm]);
+
+  const togglePlay = () => {
+    setPlaying((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h1>{bpm}</h1>
+        <button onClick={togglePlay}>{isPlaying ? 'Stop' : 'Start'}</button>
+      </div>
     </div>
   );
 }
