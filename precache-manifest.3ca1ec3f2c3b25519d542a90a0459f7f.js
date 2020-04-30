536a9710fde000ee49fa3c026cdc4ec041533dd5
David Rose <doppler@gmail.com>
2020-04-29 22:00:22 -0400

    Updates

--- precache-manifest.3ca1ec3f2c3b25519d542a90a0459f7f.js
+++ precache-manifest.3ca1ec3f2c3b25519d542a90a0459f7f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0bcbb03780bd7fa54e97910578dfcca",
+    "revision": "89937b13b747f6d55c0dd172c7cda46d",
     "url": "/metronome/index.html"
   },
   {
-    "revision": "b61f8743cc4f0a91bb09",
+    "revision": "34eebc663867d4d90b3a",
     "url": "/metronome/static/css/main.3a0b1b80.chunk.css"
   },
   {
-    "revision": "00d5e477044c31ed2161",
-    "url": "/metronome/static/js/2.f2cdb5be.chunk.js"
+    "revision": "668cc0ed10c7a65a598a",
+    "url": "/metronome/static/js/2.87cee6b9.chunk.js"
   },
   {
     "revision": "e58b2fed1a83725c5b0108382043ede5",
-    "url": "/metronome/static/js/2.f2cdb5be.chunk.js.LICENSE.txt"
+    "url": "/metronome/static/js/2.87cee6b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b61f8743cc4f0a91bb09",
-    "url": "/metronome/static/js/main.3e0b0809.chunk.js"
+    "revision": "34eebc663867d4d90b3a",
+    "url": "/metronome/static/js/main.34fff9bb.chunk.js"
   },
   {
     "revision": "e098453104613217ecba",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/metronome/precache-manifest.5638e299e079d6140d15db89d72d416e.js"
+  "/metronome/precache-manifest.3ca1ec3f2c3b25519d542a90a0459f7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/metronome/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
